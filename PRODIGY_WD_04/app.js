5080e1a6729b625ad32250f87e01d994e765c872
SooryaDevil <145997858+SooryaDevil@users.noreply.github.com>
2025-06-17 22:07:25 +0530

    Update app.js

--- PRODIGY_WD_04/app.js
+++ PRODIGY_WD_04/app.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
   if (resumeBtn) {
     resumeBtn.addEventListener('click', function(e) {
       e.preventDefault();
-      window.open('https://drive.google.com/file/d/1hDpObXCUtYVDlZjThsj-QKY6QlZAAcHS/view?usp=drive_link', '_blank');
+      window.open('https://raw.githubusercontent.com/SooryaDevil/PRODIGY_WD_04/main/PRODIGY_WD_04/Soorya%20Dev-%20resume.pdf', '_blank');
     });
   }
 
   // Github button
   const githubBtn = document.querySelector('.custom-btn[target="_blank"]');
   if (githubBtn) {
-    githubBtn.setAttribute('href', 'https://github.com/Karthik20643');
+    githubBtn.setAttribute('href', 'https://github.com/SooryaDevil');
   }
 });
